41079531c1d46a835e9e784843bb130365bd9296
Rohan <rohan.nandode@coriolis.co.in>
2012-11-28 19:44:56 +0530

    - Updated commandline to insert qemu-pre.js

---
+++ qemu-pre.js
@@ -0,0 +1,8 @@
+
+//add some undefined vars, come up through emscripten compilation.
+if(_get_human_readable_size_suffixes === undefined) 
+    var _get_human_readable_size_suffixes = '';
+
+if(_vmdk_create_desc_template === undefined)
+    var _vmdk_create_desc_template = '';
+
